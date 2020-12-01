d856f4fec127a108a8769ace30584f539bf7d39e
hoxovic <9694402+hoxovic@users.noreply.github.com>
2020-12-01 17:15:43 +0100

    Document read_input properly

--- inputreader.py
+++ inputreader.py
@@ -10,7 +10,21 @@ def read_input(
     transform_function: Callable[[str], Any] = _passthrough,
     filepath: str = "input.txt",
 ) -> Generator[Any, None, None]:
-    """Reads the input line by line from filepath"""
+    """Reads the input line by line from filepath
+
+    Parameters
+    ----------
+    transform_function
+        each line in the file is passed to this function and
+        the return value is yielded. default: direct return of input
+    filepath
+        absolute or relative path to input file
+
+    Yields
+    ------
+    any
+        Each line transformed by `transform_function`
+    """
     with open(filepath) as fh:
         for line in fh:
             yield transform_function(line)
