84d9c0f8ad0c0c22b2d6f404bf48980632c719d2
hoxovic <9694402+hoxovic@users.noreply.github.com>
2020-12-01 17:26:06 +0100

    Add default no-op transform_function to template

--- template/template.py
+++ template/template.py
@@ -6,17 +6,22 @@ sys.path.append("..")
 from inputreader import read_input  # pylint: disable=import-error
 
 
-def part1():
+def transform_function(line):
+    return line
+
+
+def part1(data):
     pass
 
 
-def part2():
+def part2(data):
     pass
 
 
 def main():
-    print(f"part1: {part1()}")
-    print(f"part2: {part2()}")
+    data = tuple(read_input(transform_function=transform_function))
+    print(f"part1: {part1(data)}")
+    print(f"part2: {part2(data)}")
 
 
 if __name__ == "__main__":
