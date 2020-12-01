5eb0dc79d3d73391bd9fe6415d979d132d82cee6
hoxovic <9694402+hoxovic@users.noreply.github.com>
2020-12-01 07:37:13 +0100

    Add boilerplate template

---
+++ template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+
+
+sys.path.append("..")
+from inputreader import read_input
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    print(f"part1: {part1()}")
+    print(f"part2: {part2()}")
+
+
+if __name__ == "__main__":
+    main()
