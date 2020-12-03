47689468d449b1c5535d6177f7720af11d90fd16
hoxovic <9694402+hoxovic@users.noreply.github.com>
2020-12-03 19:36:15 +0100

    Add input formatting function

--- template/template.py
+++ template/template.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 import sys
+from typing import Any, Generator
 
 
 sys.path.append("..")
 from inputreader import read_input  # pylint: disable=import-error
 
 
+def main():
+    lines = read_input(transform_function=transform_function)
+    data = format_input(lines)
+
+    print(f"part1: {part1(data)}")
+    print(f"part2: {part2(data)}")
+
+
 def transform_function(line):
     return line
+
+
+def format_input(lines: Generator[Any, None, None]) -> Any:
+    """Format the lines into the expected data format"""
+    return tuple(lines)
 
 
 def part1(data):
@@ -16,12 +30,6 @@ def part1(data):
 
 def part2(data):
     pass
-
-
-def main():
-    data = tuple(read_input(transform_function=transform_function))
-    print(f"part1: {part1(data)}")
-    print(f"part2: {part2(data)}")
 
 
 if __name__ == "__main__":
